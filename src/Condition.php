24609de5689b62126924a671f3c8ff73c8133cfa
Vitaly Iegorov <vitalyiegorov@gmail.com>
2015-11-30 01:27:50 +0200

    Refactored Condition

--- src/Condition.php
+++ src/Condition.php
@@ -1,48 +1,51 @@
 <?php
 namespace samsonframework\orm;
 
+use samsonframework\orm\ConditionInterface;
+use samsonframework\orm\ArgumentInterface;
+
 /**
  * Query condition arguments group
  * @author Vitaly Iegorov <egorov@samsonos.com>
  * @version 2.0
  */
-class Condition
+class Condition implements ConditionInterface
 {
-    /** AND(conjunction) - Condition relation type */
-    const REL_AND = 'AND';
-
-    /** OR(disjunction) - Condition relation type */
-    const REL_OR = 'OR';
-
     /** @var Argument[] Collection of condition arguments */
     public $arguments = array();
 
     /** @var string Relation logic between arguments */
-    public $relation = self::REL_AND;
+    public $relation = ConditionInterface::CONJUNCTION;
 
     /**
      * Add condition argument to this condition group
      * @param ArgumentInterface $argument Condition argument to be added
+     * @return self Chaining
      */
     public function addArgument(ArgumentInterface $argument)
     {
         // Add condition as current condition argument
         $this->arguments[] = $argument;
+
+        return $this;
     }
 
     /**
      * Add condition group to this condition group
      * @param self $condition Condition group to be added
+     * @return self Chaining
      */
     public function addCondition(self $condition)
     {
         // Add condition as current condition argument
         $this->arguments[] = $condition;
+
+        return $this;
     }
 
     /**
      * Generic condition addiction function
-     * @param self|Argument|string $argument Entity for adding to arguments collection
+     * @param string $argument Entity for adding to arguments collection
      * @param string $value Argument value
      * @param string $relation Relation between argument and value
      * @return self Chaining
@@ -52,10 +55,6 @@ class Condition
         if (is_string($argument) || is_scalar($argument)) {
             // Add new argument to arguments collection
             $this->arguments[] = new Argument($argument, $value, $relation);
-        } elseif (is_a($argument, get_class($this))) {
-            $this->addCondition($argument);
-        } elseif (is_a($argument, __NAMESPACE__.'Argument')) {
-            $this->addArgument($argument);
         }
 
         return $this;
@@ -67,6 +66,6 @@ class Condition
      */
     public function __construct($relation = null)
     {
-        $this->relation = isset($relation) ? $relation : self::REL_AND;
+        $this->relation = isset($relation) ? $relation : ConditionInterface::CONJUNCTION;
     }
 }
