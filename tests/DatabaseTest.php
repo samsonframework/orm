f5c312e6ce66ef17ab0380c11c79c735a7aaae57
Vitaly Iegorov <vitalyiegorov@gmail.com>
2016-08-29 10:02:50 +0300

    Added dev branch alias

---
+++ tests/DatabaseTest.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types = 1);
+/**
+ * Created by Vitaly Iegorov <egorov@samsonos.com>.
+ * on 29.08.16 at 10:01
+ */
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class DatabaseTest.
+ *
+ * @author Vitaly Egorov <egorov@samsonos.com>
+ */
+class DatabaseTest extends TestCase
+{
+    public function test
+}
