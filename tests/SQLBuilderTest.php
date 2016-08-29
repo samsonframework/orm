4de66df28a41c737fc80d2d0611bc3b85b6053cf
Vitaly Iegorov <vitalyiegorov@gmail.com>
2016-08-29 17:44:02 +0300

    Addind sqlbuilder tests

--- tests/SQLBuilderTest.php
+++ tests/SQLBuilderTest.php
@@ -7,6 +7,7 @@ namespace samsonframework\orm\tests;
 
 use PHPUnit\Framework\TestCase;
 use samsonframework\orm\SQLBuilder;
+use samsonframework\orm\TableMetadata;
 
 /**
  * Class SQLBuilderTest
@@ -25,6 +26,37 @@ class SQLBuilderTest extends TestCase
 
     public function testBuildSelectStatement()
     {
+        $metadata = new TableMetadata();
+        $metadata->tableName = 'testTable';
+        $metadata->className = TestEntity::class;
+        $metadata->columns[] = 'testColumn';
+        $metadata->columns[] = 'testColumn2';
 
+        static::assertEquals(
+            'SELECT `testTable`.`testColumn`, `testTable`.`testColumn2`',
+            $this->sqlBuilder->buildSelectStatement($metadata)
+        );
+    }
+
+    public function testBuildSelectStatementWithJoins()
+    {
+        $metadata = new TableMetadata();
+        $metadata->tableName = 'testTable';
+        $metadata->className = TestEntity::class;
+        $metadata->columns[] = 'testColumn';
+        $metadata->columns[] = 'testColumn2';
+
+        $joinedMetadata = [];
+        $joinedMetadata[0] = new TableMetadata();
+        $joinedMetadata[0]->tableName = 'testTable2';
+        $joinedMetadata[0]->className = JoinTestEntity::class;
+        $joinedMetadata[0]->columns[] = 'testColumn3';
+        $joinedMetadata[0]->columns[] = 'testColumn4';
+
+        static::assertEquals(
+            'SELECT `testTable`.`testColumn`, `testTable`.`testColumn2`'.
+            "\n".'`testTable2`.`testColumn3`, `testTable2`.`testColumn4`',
+            $this->sqlBuilder->buildSelectStatement($metadata, $joinedMetadata)
+        );
     }
 }
